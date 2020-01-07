af2fa21edb1a0bbf95d79d65c06920d64250b6b1
Thomas Maitret <t.maitret-richard@groupeonepoint.com>
2020-01-07 15:56:12 +0100

    specify favicon size in request

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ const appendListingsToDOM = listings => {
     const icon = document.createElement("img");
     icon.alt = `${data.domain} favicon`;
     const domainURL = data.domain.startsWith('self.') ? 'reddit.com' : data.domain;
-    icon.src = `https://www.google.com/s2/favicons?domain_url=${domainURL}`;
+    icon.src = `https://www.google.com/s2/favicons?domain_url=${domainURL}&sz=16`;
     icon.classList.add("site-icon");
 
     const li = document.createElement("li");
