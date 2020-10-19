1764ef7f4e8715d4508d5dc1e6dd5928fc13e7f7
Thomas Maitret Richard <thomas.maitret@laposte.net>
2020-10-19 15:05:58 +0200

    custom build, remove parcel

---
+++ bundle.js
@@ -0,0 +1,27 @@
+import minify from "@node-minify/core";
+import htmlMinifier from "@node-minify/html-minifier";
+import cssnano from "@node-minify/cssnano";
+import uglifyES from "@node-minify/uglify-es";
+import fs from "fs-extra";
+
+const DIRECTORY = "dist";
+
+fs.emptyDirSync(DIRECTORY);
+fs.ensureDirSync(DIRECTORY);
+fs.copySync("./public", DIRECTORY);
+
+minify({
+  compressor: uglifyES,
+  input: "./src/main.js",
+  output: `./${DIRECTORY}/main.js`,
+});
+minify({
+  compressor: htmlMinifier,
+  input: "./src/index.html",
+  output: `./${DIRECTORY}/index.html`,
+});
+minify({
+  compressor: cssnano,
+  input: "./src/style.css",
+  output: `./${DIRECTORY}/style.css`,
+});
