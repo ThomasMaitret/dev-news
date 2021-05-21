e74cb1986ad4cb12dc31df226bc80b9e6c5c62ce
Thomas Maitret Richard <thomas.maitret@laposte.net>
2021-05-21 18:19:53 +0200

    update deps

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -1,8 +1,4 @@
-// Snowpack Configuration File
-// See all supported options: https://www.snowpack.dev/reference/configuration
-
-/** @type {import("snowpack").SnowpackUserConfig } */
-module.exports = {
+export default {
   mount: {
     src: "/",
   },
